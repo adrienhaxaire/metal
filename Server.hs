2e12f68411fa70e9536e5e60f74b877e5e2e2d1f
Adrien Haxaire <adrien@haxaire.org>
2013-07-15 01:58:32 +0200

    preparing better request handling

--- Server.hs
+++ Server.hs
@@ -4,22 +4,53 @@ import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Monad
 import Control.Monad.STM
+import qualified Data.ByteString as B
 
-type Response = TVar String
+data Resp = Resp { respHeader :: [String] 
+                 , respData :: B.ByteString}
+           deriving (Eq, Ord, Show)
+type Response = TVar Resp
 
-prepareResponse :: String -> STM Response
-prepareResponse s = newTVar $
-                    "HTTP/1.1 200 OK \nContent-Type: text/plain\n\n"
-                    ++ s ++ "\n"
+data Request = Request { reqHeader :: [String] 
+                       , reqData :: B.ByteString}
+               deriving (Eq, Ord, Show)
 
-showResponse :: String -> IO String
-showResponse lines = atomically $ prepareResponse lines >>= readTVar
+-- have to parse request properly
+
+emptyRequest :: Request
+emptyRequest = Request [] B.empty
+
+hasRequestMethod :: String -> Request -> Bool
+hasRequestMethod m req = any (\l -> (take $ length m) l == m) $ reqHeader req
+
+isPostRequest :: Request -> Bool
+isPostRequest = hasRequestMethod "POST"
+
+isGetRequest :: Request -> Bool
+isGetRequest = hasRequestMethod "GET"
+
+addToRequestHeader :: String -> Request -> Request
+addToRequestHeader line req = req {reqHeader = reqHeader req ++ [init line]}
+
+readRequest :: Handle -> IO Request
+readRequest handle = go handle emptyRequest
+    where 
+      go h r = do
+        isReady <- hReady handle
+        if isReady 
+        then do line <- hGetLine handle
+                go h $ addToRequestHeader line r
+        else return r
+
 
 serve :: Handle -> IO ()
 serve handle = do
   hSetBuffering handle NoBuffering
-  hGetLine handle >>= showResponse >>= hPutStr handle
-  hClose handle
+  request <- readRequest handle
+  print request
+  hClose handle -- do not close if keep alive
+--  response <- respond request
+--  hPutStr responseHeader
 
 server :: IO ()
 server = withSocketsDo $ do
@@ -29,5 +60,4 @@ server = withSocketsDo $ do
       forkIO $ serve handle
 
 main = server
-
 
